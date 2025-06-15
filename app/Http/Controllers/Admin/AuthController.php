6e1133b12c8895b27e154367039e15ed528b0dfc
zaynab10o <zaynabm766@gmail.com>
2025-06-15 13:05:53 +0300

    Add initial controllers and request validation for admin features
    - Created CarController, DriverController, and ModelController in the Admin namespace.
    - Added login and register request validation classes in the Admin namespace.
    - Implemented Arabic, English, and Russian language files for authentication, pagination, passwords, and validation messages.

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Admin\Register;
+use App\Http\Requests\Admin\Login;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Register $request)
+    {
+        $validatedData = $request->validated();
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $admin = Admin::create($validatedData);
+
+        return response()->json([
+            'message' => 'Admin registered successfully.',
+            'admin' => $admin,
+        ], 201);
+    }
+
+    public function loginUser(Login $request)
+    {
+        $credentials = $request->validated();
+
+    
+        $admin = Admin::where('email', $credentials['email'])->first();
+
+        if (! $admin || ! Hash::check($credentials['password'], $admin->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+    
+        $token = $admin->createToken('admin-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'admin' => $admin,
+            'token' => $token,
+        ]);
+    }
+
+
+    public function logout(Request $request)
+    {
+        $admin = Auth::guard('admin')->user();
+
+        if ($admin) {
+            $admin->tokens()->delete();
+            return response()->json(['message' => 'Logged out successfully']);
+        }
+
+        return response()->json(['message' => 'No admin logged in'], 401);
+    }
+}
